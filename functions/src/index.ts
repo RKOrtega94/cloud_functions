172f1ee539e32c89e69196f47fe5f9004b15b7c8
RKOrtega <rkortega1994@gmail.com>
2025-01-27 19:19:31 -0500

    Refactor ESLint configuration: remove old config files, add new config, and integrate CORS middleware for improved API handling

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,8 @@ import * as bodyParser from "body-parser";
 import { onRequest } from "firebase-functions/v2/https";
 import * as admin from "firebase-admin";
 
+const cors = require("cors")({ origin: true });
+
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
 });
@@ -13,8 +15,9 @@ import TASK from "./modules/tasks/adapters/rest/task_controller";
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors);
 
 app.use("/users", USER);
 app.use("/tasks", TASK);
 
-export const api = onRequest(app);
+export const api = onRequest({ cors: ["*"] }, app);
